5bd9a4386ebf2d8e77918b9c59f436d28ff1ef2f
Michael Stevan Lapandio <michaellapandio04@gmail.com>
2023-08-24 23:05:24 +0700

    learning done at 24/7/23

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
 import { useState } from "react";
 import BookCreate from "./components/BookCreate";
+import BookList from "./components/BookList";
 
 function App() {
     const [books, setBooks] = useState([]);
+    const deleteBookById = (id) => {
+        const updatedBooks = books.filter((book) => {
+            return book.id !== id
+        });
+        setBooks(updatedBooks);
+    };
 
     const createBook = (title) => {
-        console.log('Need to add book with:', title);
+        // BAD CODE!
+        // books.push({ id: 123, title: title });
+        // console.log(books);
+        const updatedBooks = [
+            ...books, 
+            { id: Math.round(Math.random()*9999), title: title 
+        }];
+        setBooks(updatedBooks);
     };
     
-    return <div>
+    return( 
+    <div className="app">
+        <BookList books={books} onDelete={deleteBookById} />
         <BookCreate onCreate={createBook} />
-    </div>;
+    </div>
+    );
 }
 
 export default App;
