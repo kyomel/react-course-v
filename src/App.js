150d5e26c5065c6e66ea8474aa20a0f24979164a
Michael Stevan Lapandio <michaellapandio04@gmail.com>
2023-08-29 23:43:28 +0700

    done learning at 29/8/2023

--- src/App.js
+++ src/App.js
@@ -4,6 +4,19 @@ import BookList from "./components/BookList";
 
 function App() {
     const [books, setBooks] = useState([]);
+
+    const updateBookById = (id, newTitle) => {
+        const updatedBooks = books.map((book) => {
+            if(book.id === id) {
+                return { ...book, title: newTitle };
+            };
+
+            return book;
+        });
+
+        setBooks(updatedBooks);
+    };
+
     const deleteBookById = (id) => {
         const updatedBooks = books.filter((book) => {
             return book.id !== id
@@ -24,7 +37,8 @@ function App() {
     
     return( 
     <div className="app">
-        <BookList books={books} onDelete={deleteBookById} />
+        <h1>Reading List</h1>
+        <BookList onEdit={updateBookById} books={books} onDelete={deleteBookById} />
         <BookCreate onCreate={createBook} />
     </div>
     );
--- src/components/BookShow.js
+++ src/components/BookShow.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import BookEdit from "./BookEdit";
 
-function BookShow({book, onDelete}) {
+function BookShow({book, onDelete, onEdit}) {
     const [showEdit, setShowEdit] = useState(false);
     const handleDeleteClick = () => {
         onDelete(book.id);
@@ -10,12 +10,21 @@ function BookShow({book, onDelete}) {
         setShowEdit(!showEdit);
     };
 
+    const handleSubmit = (id, newTitle) => {
+        setShowEdit(false);
+        onEdit(id, newTitle);
+    };
+
     let content = <h3>{book.title}</h3>;
     if(showEdit) {
-        content = <BookEdit />
+        content = <BookEdit onSubmit={handleSubmit} book={book}/>
     }
 
     return <div className="book-show">
+        <img
+            alt="books"
+            src={`https://picsum.photos/seed/${book.id}/300/200`}
+        />
         <div>
             {content}
         </div>
