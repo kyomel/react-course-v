2f68b50c1dc9c0733a4fd5ad62d4d3e2dc5e249d
Michael Stevan Lapandio <michaellapandio04@gmail.com>
2023-09-19 06:12:28 +0700

    done learning at 19/9/2023

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
 import axios from "axios";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BookCreate from "./components/BookCreate";
 import BookList from "./components/BookList";
 
 function App() {
     const [books, setBooks] = useState([]);
 
-    const updateBookById = (id, newTitle) => {
+    const fetchBooks = async () => {
+        const response = await axios.get("http://localhost:3001/books");
+
+        setBooks(response.data);
+    };
+
+    // Don't Do This
+    // fetchBooks();
+
+    useEffect(() => {
+        fetchBooks();
+    }, []);
+
+    const updateBookById = async(id, newTitle) => {
+        const response = await axios.put(`http://localhost:3001/books/${id}`, { 
+            title: newTitle 
+        });
+
         const updatedBooks = books.map((book) => {
             if(book.id === id) {
-                return { ...book, title: newTitle };
+                return { ...book, ...response.data };
             };
 
             return book;
@@ -18,7 +35,9 @@ function App() {
         setBooks(updatedBooks);
     };
 
-    const deleteBookById = (id) => {
+    const deleteBookById = async(id) => {
+        await axios.delete(`http://localhost:3001/books/${id}`);
+        
         const updatedBooks = books.filter((book) => {
             return book.id !== id
         });
